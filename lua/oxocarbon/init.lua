eee2d2caffe7d89825e367b5163aaff2776d6df3
Erdogan, Kevin <kerdogan@lbisoftware.com>
2025-04-24 21:40:42 -0400

    reverted

--- lua/oxocarbon/init.lua
+++ lua/oxocarbon/init.lua
@@ -285,7 +285,7 @@ vim.api.nvim_set_hl(0, "TelescopeBorder", {fg = oxocarbon.base01, bg = oxocarbon
 vim.api.nvim_set_hl(0, "TelescopePromptBorder", {fg = oxocarbon.base02, bg = oxocarbon.base00})
 vim.api.nvim_set_hl(0, "TelescopePromptNormal", {fg = oxocarbon.base02, bg = oxocarbon.base00})
 vim.api.nvim_set_hl(0, "TelescopePromptPrefix", {fg = oxocarbon.base08, bg = oxocarbon.base02})
-vim.api.nvim_set_hl(0, "TelescopeNormal", {fg = oxocarbon.base01, bg = oxocarbon.base00})
+vim.api.nvim_set_hl(0, "TelescopeNormal", {fg = oxocarbon.none, bg = oxocarbon.blend})
 vim.api.nvim_set_hl(0, "TelescopePreviewTitle", {fg = oxocarbon.base02, bg = oxocarbon.base12})
 vim.api.nvim_set_hl(0, "TelescopePromptTitle", {fg = oxocarbon.base02, bg = oxocarbon.base11})
 vim.api.nvim_set_hl(0, "TelescopeResultsTitle", {fg = oxocarbon.blend, bg = oxocarbon.blend})
